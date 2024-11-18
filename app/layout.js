203f7b638a41fdb8051e0f74d960832d1d0bd694
Subhampreet <subhampreet18@gmail.com>
2024-11-18 21:23:18 +0530

    Build 0.0.1 : Application Init

---
+++ app/layout.js
@@ -0,0 +1,45 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
+import { Inter } from 'next/font/google';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
+export const metadata = {
+  title: "ZCRUM",
+  description: "Project Management Tool",
+};
+
+const inter = Inter({ subsets: ['latin'] });
+
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={inter.className}
+        >
+          <ThemeProvider attribute="class" defaultTheme="dark" >
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+            <main className="min-h-screen">{children}</main>
+            <footer className="bg-gray-900 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>developed by subhampreet</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
